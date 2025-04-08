e64ab2b827e645e063754095548db4ddf85ea036
cuevasarancibia <166886956+cuevasarancibia@users.noreply.github.com>
2025-04-08 15:21:18 -0400

    Update app.py
    app.run(host='0.0.0.0', port=8000)
    
    eso cambio

--- app.py
+++ app.py
@@ -181,4 +181,4 @@ def exportar():
     return send_file(output, download_name="clientes_filtrados.xlsx", as_attachment=True)
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5050)
+    app.run(host='0.0.0.0', port=8000)
